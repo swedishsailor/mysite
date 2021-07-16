925644b8a4c46117b07ac4e16bc9253985c61a6e
swedishsailor <kzygowski7@gmail.com>
2021-07-16 16:45:42 +0200

    npm init

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './App.scss';
+import PropTypes from 'prop-types';
+
+class App extends React.Component{
+    render(){
+        return(
+            
+                <main>
+                    <h1> XDD </h1>
+                </main>
+            );
+        }
+    }
+
+    export default App;
