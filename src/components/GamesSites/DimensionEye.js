81b54bf8802f246ee576b7cbfd3e83d173c1fce1
swedishsailor <kzygowski7@gmail.com>
2021-08-24 19:44:59 +0200

    webgl still doesnt work

--- src/components/GamesSites/DimensionEye.js
+++ src/components/GamesSites/DimensionEye.js
@@ -1,9 +1,27 @@
 import React from "react";
+import Unity, { UnityContext } from "react-unity-webgl";
+
+const unityContext = new UnityContext({
+    loaderUrl: 'WebDimensionEyeDEMO/Build/UnityLoader.js',
+    dataUrl: 'WebDimensionEyeDEMO/Build/WebDimensionEyeDEMO.data.unityweb',
+    frameworkUrl: 'WebDimensionEyeDEMO/Build/WebDimensionEyeDEMO.wasm.framework.unityweb',
+    codeUrl: 'WebDimensionEyeDEMO/Build/WebDimensionEyeDEMO.wasm.code.unityweb',
+  });
 
 const DimensionEye = () => (
+    
     <div id='/dimensioneye'>
-        <p> XDDDDDDDDDDDDDDD </p>
+        <Unity unityContext={unityContext}
+ />
     </div>
 );
 
 export default DimensionEye;
+
+/*
+
+    loaderUrl: './WebDimensionEyeDEMO/Build/UnityLoader.js',
+    dataUrl: './WebDimensionEyeDEMO/Build/WebDimensionEyeDEMO.data.unityweb',
+    frameworkUrl: './WebDimensionEyeDEMO/Build/WebDimensionEyeDEMO.wasm.framework.unityweb',
+    codeUrl: './WebDimensionEyeDEMO/Build/WebDimensionEyeDEMO.wasm.code.unityweb',
+    */
